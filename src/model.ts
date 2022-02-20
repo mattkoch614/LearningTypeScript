3b9ff80588517dea2f5d1828d158b7a6fb51f9fc
mattkoch614 <matthewjameskoch@gmail.com>
2022-02-20 15:03:01 -0500

    complex types

--- src/model.ts
+++ src/model.ts
@@ -1,1 +1,28 @@
 let displayname: string = "Matt's desk";
+let inventoryType: string = "furniture";
+let trackingNumber: string = "FD123455";
+let createDate: Date = new Date();
+let originalCost: any = 425;
+originalCost = "OMG! LOTS OF CASH!"
+
+function getInventoryItem(trackingNumber: string) : {
+    displayName: string;
+    inventoryType: string;
+    trackingNumber: string;
+    createDate: Date;
+    originalCost: number;
+} {
+    return null;
+}
+
+function saveInventoryItem(item) {
+
+}
+
+let inventoryItem = getInventoryItem(trackingNumber);
+
+let updatedInventoryItem = inventoryItem;
+
+inventoryItem.createDate = new Date();
+
+saveInventoryItem(inventoryItem);
