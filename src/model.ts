05c256f55aa6fa511a17cbe4ee68ab890073cbe8
mattkoch614 <matthewjameskoch@gmail.com>
2022-02-20 15:05:58 -0500

    Defining custom types with interfaces

--- src/model.ts
+++ src/model.ts
@@ -2,21 +2,22 @@ let displayname: string = "Matt's desk";
 let inventoryType: string = "furniture";
 let trackingNumber: string = "FD123455";
 let createDate: Date = new Date();
-let originalCost: any = 425;
-originalCost = "OMG! LOTS OF CASH!"
+let originalCost = 425;
 
-function getInventoryItem(trackingNumber: string) : {
+interface InventoryItem {
     displayName: string;
     inventoryType: string;
     trackingNumber: string;
     createDate: Date;
     originalCost: number;
-} {
+}
+
+function getInventoryItem(trackingNumber: string) : InventoryItem {
     return null;
 }
 
-function saveInventoryItem(item) {
-
+function saveInventoryItem(item: InventoryItem) {
+    
 }
 
 let inventoryItem = getInventoryItem(trackingNumber);
